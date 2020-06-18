eb1b458f5daacf059775a3e0fc2c79bc758ea06c
Renato <renatoelysiqueira@gmail.com>
2020-06-18 01:33:43 -0300

    fix

--- testar-planilha.js
+++ testar-planilha.js
@@ -1,17 +1,17 @@
-const GoogleSpreadsheet = require('google-spreadsheet')
-const credentials = require('./bugtracker.json')
-const { promisify } = require('util')
+// const GoogleSpreadsheet = require('google-spreadsheet')
+// const credentials = require('./bugtracker.json')
+// const { promisify } = require('util')
 
-const doc = new GoogleSpreadsheet()
-doc.useServiceAccountAuth(credentials, (err) => {
-    if (err) {
-        console.log('Não foi possível abrir a planilha')
-    } else {
-        doc.getInfo((err, info) => {
-            const worksheet = info.worksheets[0]
-            worksheet.addRow({ name: '', email: 'teste' }, err => {
-                console.log('Linha Inserida')
-            })
-        })
-    }
-})
+// const doc = new GoogleSpreadsheet()
+// doc.useServiceAccountAuth(credentials, (err) => {
+//     if (err) {
+//         console.log('Não foi possível abrir a planilha')
+//     } else {
+//         doc.getInfo((err, info) => {
+//             const worksheet = info.worksheets[0]
+//             worksheet.addRow({ name: '', email: 'teste' }, err => {
+//                 console.log('Linha Inserida')
+//             })
+//         })
+//     }
+// })
